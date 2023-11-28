2ce386dc402705bc16226ba7092ca61b53eeb543
Kunj Ukani <kunj.ukani@idfcfirstbank.com>
2023-11-28 12:46:30 +0530

    Kunj|Gourav: Added Card to show book

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
-import BookList from './BookList/BookList';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import BookList from './BookList/BookList';
+import Header from './Header/Header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          BookShop Demo 2
-        </p>
+        <Header />
         <BookList />
       </header>
     </div>
--- src/BookList/BookList.js
+++ src/BookList/BookList.js
@@ -1,13 +1,40 @@
 import BookCard from './BookCard/BookCard';
+import "./BookList.styles.css";
 
-function BookList(book) {
-    const bookList = [{
-        "name": "Test"
-    }];
+function BookList() {
+    const bookList = [
+        {
+            "name": "Test 1",
+            "desc": "This is the description of test 1"
+        },
+        {
+            "name": "Test 2",
+            "desc": "This is the description of test 2"
+        },
+        {
+            "name": "Test 3",
+            "desc": "This is the description of test 3"
+        },
+        {
+            "name": "Test 4",
+            "desc": "This is the description of test 4"
+        },
+        {
+            "name": "Test 5",
+            "desc": "This is the description of test 5"
+        }
+
+    ];
 
     return (
-        <BookCard book={bookList[0]} />
+        <div class="bookCardParent">
+            {bookList.map(book => (
+                <div class="bookCard">
+                    <BookCard book={book} />
+                </div>
+            ))}
+        </div>
     );
-  }
+}
 
 export default BookList;
