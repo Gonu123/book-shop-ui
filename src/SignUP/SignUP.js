d94fc12e73df7955acf14b5acf18b01187da6a0c
absolute.naman <absolute.naman@gmail.com>
2023-11-29 20:11:38 +0530

    Red build resolved

--- src/SignUP/SignUP.js
+++ src/SignUP/SignUP.js
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import "../SignUP/style.css";
 import { Navigate } from "react-router";
 import axios from "axios";
@@ -9,8 +9,7 @@ const SignUp = () => {
   const [name, setName] = useState("");
 
   const SignUPUrl = "http://localhost:8080/idp/create-user";
-  const passwordRegex =
-    /^(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/\-])(?=.*\d)(?=.*[A-Z]).{8,16}$/;
+
   const Emailpattern = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
 
   async function OnSubmit(async) {
@@ -20,7 +19,7 @@ const SignUp = () => {
       phNo: mobileNumber,
     });
 
-    if (!mobileNumber.length == 10) {
+    if (!mobileNumber.length === 10) {
       alert("Phone Number Validation failed");
     } else if (!Emailpattern.test(email)) {
       alert("Email validation failed");
@@ -35,131 +34,130 @@ const SignUp = () => {
           console.log(response);
         })
         .catch((err) => {
-          alert("Username Already Taken")
+          alert("Username Already Taken");
         });
       return <Navigate to="/" replace={true} />;
     }
   }
 
-  const SignUP = () => {
-    return (
-      <div>
-        <section class="h-100 gradient-form" style={{ backgroundColor: "#eee" }}>
-          <div class="container py-5 h-100">
-            <div class="row d-flex justify-content-center align-items-center h-100">
-              <div class="col-xl-10">
-                <div class="card rounded-3 text-black">
-                  <div class="row g-0">
-                    <div class="col-lg-6">
-                      <div class="card-body p-md-5 mx-md-4">
-                        <div class="text-center">
-                          <img
-                            src="https://upload.wikimedia.org/wikipedia/commons/7/7b/IDFC_First_Bank_logo.jpg"
-                            style={{ width: 185 }}
-                            alt="logo"
-                          />
-                          <h4 class="mt-1 mb-5 pb-1">
-                            We are The Invincible Team
-                          </h4>
-                        </div>
-
-                        <form >
-                          <p>Please login to your account</p>
-
-                          <div class="form-outline mb-4">
-                            <input
-                              type="email"
-                              class="form-control"
-                              id="exampleInputEmail1"
-                              aria-describedby="emailHelp"
-                            />
-                            <label class="form-label" for="form2Example11">
-                              Username
-                            </label>
-                          </div>
-
-                          <div class="form-outline mb-4">
-                            <input
-                              type="password"
-                              id="form2Example22"
-                              class="form-control"
-                            />
-                            <label class="form-label" for="form2Example22">
-                              Password
-                            </label>
-                          </div>
-
-                          <div class="text-center pt-1 mb-5 pb-1">
-                            <button
-                              class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3"
-                              type="submit"
-                            >
-                              Log in
-                            </button>
-                            {/* <a class="text-muted" href="#!">
-                            Forgot password?
-                          </a> */}
-                          </div>
-
-                          <div class="d-flex align-items-center justify-content-center pb-4">
-                            <p class="mb-0 me-2">Don't have an account?</p>
-                            <button type="button" class="btn btn-outline-danger">
-                              Create new
-                            </button>
-                          </div>
-                        </form>
+  return (
+    <div>
+      <section class="h-100 gradient-form" style={{ backgroundColor: "#eee" }}>
+        <div class="container py-5 h-100">
+          <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-xl-10">
+              <div class="card rounded-3 text-black">
+                <div class="row g-0">
+                  <div class="col-lg-6">
+                    <div class="card-body p-md-5 mx-md-4">
+                      <div class="text-center">
+                        <img
+                          src="https://upload.wikimedia.org/wikipedia/commons/7/7b/IDFC_First_Bank_logo.jpg"
+                          style={{ width: 185 }}
+                          alt="logo"
+                        />
+                        <h4 class="mt-1 mb-5 pb-1">
+                          We are The Invincible Team
+                        </h4>
                       </div>
 
-                      <form id="registrationForm">
-                        <div class="mb-3">
-                          <label for="name" class="form-label">
-                            Name:
-                          </label>
-                          <input
-                            type="text"
-                            class="form-control"
-                            id="name"
-                            name="name"
-                            required
-                            onChange={(event) => {
-                              setName(event.target.value);
-                            }}
-                          />
-                        </div>
-                        <div class="mb-3">
-                          <label for="mobile" class="form-label">
-                            Mobile Number:
-                          </label>
-                          <input
-                            type="text"
-                            class="form-control"
-                            id="mobile"
-                            name="mobile"
-                            pattern="[0-9]{10}"
-                            required
-                            onChange={(event) => {
-                              setMobileNumber(event.target.value);
-                            }}
-                          />
-                        </div>
+                      <form>
+                        <p>Please login to your account</p>
 
-                        <div class="mb-3">
-                          <label for="email" class="form-label">
-                            Email/Username
-                          </label>
+                        <div class="form-outline mb-4">
                           <input
                             type="email"
                             class="form-control"
-                            id="email"
-                            name="email"
-                            required
-                            onChange={(event) => {
-                              SetEmail(event.target.value);
-                            }}
+                            id="exampleInputEmail1"
+                            aria-describedby="emailHelp"
                           />
+                          <label class="form-label" for="form2Example11">
+                            Username
+                          </label>
                         </div>
 
-                        {/* <div class="mb-3">
+                        <div class="form-outline mb-4">
+                          <input
+                            type="password"
+                            id="form2Example22"
+                            class="form-control"
+                          />
+                          <label class="form-label" for="form2Example22">
+                            Password
+                          </label>
+                        </div>
+
+                        <div class="text-center pt-1 mb-5 pb-1">
+                          <button
+                            class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3"
+                            type="submit"
+                          >
+                            Log in
+                          </button>
+                          {/* <a class="text-muted" href="#!">
+                            Forgot password?
+                          </a> */}
+                        </div>
+
+                        <div class="d-flex align-items-center justify-content-center pb-4">
+                          <p class="mb-0 me-2">Don't have an account?</p>
+                          <button type="button" class="btn btn-outline-danger">
+                            Create new
+                          </button>
+                        </div>
+                      </form>
+                    </div>
+
+                    <form id="registrationForm">
+                      <div class="mb-3">
+                        <label for="name" class="form-label">
+                          Name:
+                        </label>
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="name"
+                          name="name"
+                          required
+                          onChange={(event) => {
+                            setName(event.target.value);
+                          }}
+                        />
+                      </div>
+                      <div class="mb-3">
+                        <label for="mobile" class="form-label">
+                          Mobile Number:
+                        </label>
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="mobile"
+                          name="mobile"
+                          pattern="[0-9]{10}"
+                          required
+                          onChange={(event) => {
+                            setMobileNumber(event.target.value);
+                          }}
+                        />
+                      </div>
+
+                      <div class="mb-3">
+                        <label for="email" class="form-label">
+                          Email/Username
+                        </label>
+                        <input
+                          type="email"
+                          class="form-control"
+                          id="email"
+                          name="email"
+                          required
+                          onChange={(event) => {
+                            SetEmail(event.target.value);
+                          }}
+                        />
+                      </div>
+
+                      {/* <div class="mb-3">
                         <label for="password" class="form-label">
                           Password:
                         </label>
@@ -190,36 +188,35 @@ const SignUp = () => {
                           }}
                         />
                       </div> */}
-                        <button
-                          type="button"
-                          class="btn btn-success"
-                          onClick={OnSubmit}
-                        >
-                          Register
-                        </button>
-                      </form>
-                    </div>
-                    <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
-                      <div class="text-white px-3 py-4 p-md-5 mx-md-4">
-                        <h4 class="mb-4">We are more than just a company</h4>
-                        <p class="small mb-0">
-                          Lorem ipsum dolor sit amet, consectetur adipisicing
-                          elit, sed do eiusmod tempor incididunt ut labore et
-                          dolore magna aliqua. Ut enim ad minim veniam, quis
-                          nostrud exercitation ullamco laboris nisi ut aliquip ex
-                          ea commodo consequat.
-                        </p>
-                      </div>
+                      <button
+                        type="button"
+                        class="btn btn-success"
+                        onClick={OnSubmit}
+                      >
+                        Register
+                      </button>
+                    </form>
+                  </div>
+                  <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+                    <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                      <h4 class="mb-4">We are more than just a company</h4>
+                      <p class="small mb-0">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit, sed do eiusmod tempor incididunt ut labore et
+                        dolore magna aliqua. Ut enim ad minim veniam, quis
+                        nostrud exercitation ullamco laboris nisi ut aliquip ex
+                        ea commodo consequat.
+                      </p>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </section>
-      </div>
-    );
-  };
-}
+        </div>
+      </section>
+    </div>
+  );
+};
 
 export default SignUp;
