b1eb2130570894862042cb5945401de2d6148e41
Kunj Ukani <kunj.ukani@idfcfirstbank.com>
2023-12-01 13:02:52 +0530

    Kunj|Naman: added text if order is empty

--- src/ViewOrder/ViewOrder.js
+++ src/ViewOrder/ViewOrder.js
@@ -26,40 +26,45 @@ const ViewOrder = () => {
                 View your order
             </p>
             {
-                orders.map((order) => (
-                    <div id="order">
-                        <div id="orderDetails">
-                            <h5 class="card-text">Order Id: {order.orderId}</h5>
-                            <p class="card-text">Address: {order.address}</p>
-                            <p class="card-text">Mode Of Payment: {order.modeOfPayment}</p>
-                            <p class="card-text">Order Date: {order.orderDate}</p>
-                            <p class="card-text">Total Price: ₹{order.totalPrice ? order.totalPrice!==null: 0}</p>
-                        </div>
-                        {
-                            order.items.map((item) => (
-                                <div id="book">
-                                    <div class="d-flex flex-column mb-2" >
-                                        <div class="card">
-                                            <div class="card-body d-flex flex-row d-grid gap-5">
+                orders.length > 0 ?
+                    orders.map((order) => (
+                        <div id="order">
+                            <div id="orderDetails">
+                                <h5 class="card-text">Order Id: {order.orderId}</h5>
+                                <p class="card-text">Address: {order.address}</p>
+                                <p class="card-text">Mode Of Payment: {order.modeOfPayment}</p>
+                                <p class="card-text">Order Date: {order.orderDate}</p>
+                                <p class="card-text">Total Price: ₹{order.totalPrice ? order.totalPrice !== null : 0}</p>
+                            </div>
+                            {
+                                order.items.map((item) => (
+                                    <div id="book">
+                                        <div class="d-flex flex-column mb-2" >
+                                            <div class="card">
+                                                <div class="card-body d-flex flex-row d-grid gap-5">
 
-                                                <div class="d-flex flex-column justify-content-evenly">
-                                                    <img class="card-img" id="bookImg" src={item.imageURL} alt="" />
-                                                </div>
+                                                    <div class="d-flex flex-column justify-content-evenly">
+                                                        <img class="card-img" id="bookImg" src={item.imageURL} alt="" />
+                                                    </div>
 
-                                                <div class="d-flex flex-column justify-content-evenly">
-                                                    <h5 class="card-title">Name: {item.name}</h5>
-                                                    <p class="card-text">Author: {item.subText}</p>
-                                                    <p class="card-text">Quantity: {item.quantity}</p>
-                                                    <p class="card-text">Price: ₹{item.price ? item.price!==null : 0}</p>
+                                                    <div class="d-flex flex-column justify-content-evenly">
+                                                        <h5 class="card-title">Name: {item.name}</h5>
+                                                        <p class="card-text">Author: {item.subText}</p>
+                                                        <p class="card-text">Quantity: {item.quantity}</p>
+                                                        <p class="card-text">Price: ₹{item.price ? item.price !== null : 0}</p>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            ))
-                        }
-                    </div>
-                ))
+                                ))
+                            }
+                        </div>
+                    ))
+                    :
+                    <p class="mt-2" id="noOrderText">
+                        No order placed yet, Please place order to see your orders.
+                    </p>
             }
         </>
     );
