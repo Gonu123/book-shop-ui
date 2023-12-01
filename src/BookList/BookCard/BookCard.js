ae7f3accda4a928e8e78c9989a88689e0872a0f4
Kunj Ukani <kunj.ukani@idfcfirstbank.com>
2023-12-01 14:43:06 +0530

    Kunj|Naman: update condition

--- src/BookList/BookCard/BookCard.js
+++ src/BookList/BookCard/BookCard.js
@@ -50,7 +50,7 @@ const BookCard = ({ book, cartList, setCartList }) => {
                 <path d="M5.5 8a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1-.5-.5" />
               </svg>
             </Button>
-            <input type="tel" id={`inputNumber_` + book.isbn} class="inputNumber" defaultValue="0" disabled={book.booksAvailable > 0} />
+            <input type="tel" id={`inputNumber_` + book.isbn} class="inputNumber" defaultValue="0" disabled={book.booksAvailable === 0} />
             <Button onClick={onPlusClick} class="btn btn-primary" disabled={book.booksAvailable === 0} id="minus">
               <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-plus" width="30" height="30" fill="currentColor" viewBox="4 1 16 16" >
                 <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
